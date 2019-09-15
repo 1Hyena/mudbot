c02e5b453734722bdf3e8fb137e9ca04e0c71ef4
1Hyena <hyena@hyena.net.ee>
2019-09-15 19:27:17 +0300

    Debug code.

--- agent/jesus.sh
+++ agent/jesus.sh
@@ -321,12 +321,12 @@ do
                     # and then containing '*[0m(*[1;30mHide' where * is ESC.
                     log "Skipping a hiding character: ${line}"
                 elif [[ ${line} != "No such location."* ]] ; then
-                    if [ ! -z "${line}" ] ; then
+                    #if [ ! -z "${line}" ] ; then
                         pagebuf=$(printf "%s\n%s" "${pagebuf}" "${line}")
-                    else
-                        pagebuf=$(printf "%s\n_" "${pagebuf}")
-                    fi
-                    log "${hexval} => ${line}"
+                    #else
+                    #    pagebuf=$(printf "%s\n_" "${pagebuf}")
+                    #fi
+                    #log "${hexval} => ${line}"
                 fi
             fi
 
