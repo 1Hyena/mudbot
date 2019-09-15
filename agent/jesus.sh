2bed7cac40e14aead62d917cc6628064996770f9
1Hyena <hyena@hyena.net.ee>
2019-09-15 19:08:08 +0300

    Added debug code.

--- agent/jesus.sh
+++ agent/jesus.sh
@@ -258,7 +258,9 @@ do
 
                 at=$(cut -d "#" -f 2 <<< "$EXTRA_CAM")
 
-                if [[ ${EXTRA_CAM} == "#"* ]]; then
+                log "AT: ${at}"
+
+                if [[ "${EXTRA_CAM}" == "#"* ]]; then
                     printf "at %s look\ntell self end\n" "${at}" >&${COPROC[1]}
                 else
                     printf "at %s. look\ntell self end\n" "${at}" >&${COPROC[1]}
